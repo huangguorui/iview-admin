5bd286f4acfaae75c67158a677c54f3dd588dee3
huangguorui <2315684336@qq.com>
2020-08-03 14:36:01 +0800

    更新

--- src/view/user/index.vue
+++ src/view/user/index.vue
@@ -1,75 +1,34 @@
-<style scoped>
-.layout {
-  border: 1px solid #d7dde4;
-  background: #f5f7f9;
-  position: relative;
-  border-radius: 4px;
-  overflow: hidden;
-}
-.layout-logo {
-  width: 100px;
-  height: 30px;
-  background: #5b6270;
-  border-radius: 3px;
-  float: left;
-  position: relative;
-  top: 15px;
-  left: 20px;
-}
-.layout-nav {
-  width: 420px;
-  margin: 0 auto;
-  margin-right: 20px;
-}
-.layout-footer-center {
-  text-align: center;
-}
+<style lang="less" scoped>
 </style>
 <template>
-  <div class="layout">
-    <Layout>
-      <Header :style="{position: 'fixed', width: '100%'}">
-        <Menu mode="horizontal"
-              theme="dark"
-              active-name="1">
-          <div class="layout-logo"></div>
-          <div class="layout-nav">
-            <MenuItem name="1">
-            <Icon type="ios-navigate"></Icon>
-            Item 1
-            </MenuItem>
-            <MenuItem name="2">
-            <Icon type="ios-keypad"></Icon>
+  <div>
+    {{list}}
 
-            <a href="/custom">私人定制</a>
-            </MenuItem>
-            <MenuItem name="3">
-            <Icon type="ios-analytics"></Icon>
-            Item 3
-            </MenuItem>
-            <MenuItem name="4">
-            <Icon type="ios-paper"></Icon>
-            Item 4
-            </MenuItem>
-          </div>
-        </Menu>
-      </Header>
-      <Content :style="{margin: '88px 20px 0', background: '#fff', minHeight: '500px'}">
-        <Row type="flex"
-             justify="space-between"
-             class="code-row-bg">
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-        </Row>
-      </Content>
-      <Footer class="layout-footer-center">2011-2016 &copy; TalkingData</Footer>
-    </Layout>
+    <img src="../../assets/images/login-bg.jpg">
+
+    <a href="/articleInfo">跳转</a>
+    <a href="/custom">私人定制</a>
   </div>
 </template>
-<script>
-export default {
 
+<script>
+import { getTableData } from '@/api/data'
+export default {
+  data () {
+    return {
+      list: []
+    }
+  },
+  created () {
+    console.log(getTableData().then(res => {
+      console.log(res)
+      this.list = res.data.data
+      this.pageInfo = res.data.data
+    }))
+  }
 }
+
 </script>
+
+<style lang="scss" scoped>
+</style>
